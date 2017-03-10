060eda7f43811469ddd4405b8aed74c713450c54
lchuiton <lchuiton@gmail.com>
2017-03-10 14:32:18 -0500

    Utilisation de la recherche par coordonnée de OpenWeatherMap plutôt que rechercher les données de la ville avec l’API GoogleMaps puis appeler OpenWeatherMap avec ces informations

--- src/main/java/com/weatherapp/controller/HomeController.java
+++ src/main/java/com/weatherapp/controller/HomeController.java
@@ -1,15 +1,16 @@
 package com.weatherapp.controller;
 
-import static org.springframework.util.StringUtils.isEmpty;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.weatherapp.domain.Coordinates;
 import com.weatherapp.domain.WeatherPrediction;
 import com.weatherapp.utils.exceptions.CityNotFoundException;
 
@@ -20,43 +21,7 @@ public class HomeController {
 	private String urlWeatherAPI;
 	@Value("${api.weather.key}")
 	private String keyWeatherAPI;
-	@Value("${api.googlemaps.url}")
-	private String urlGoogleMapsAPI;
-	@Value("${api.googlemaps.key}")
-	private String keyGoogleMaps;
 
-	@CrossOrigin(origins = "http://localhost:8080")
-	@RequestMapping(value = "/byCityAndCountry")
-	public WeatherPrediction listPredictionParNomDeVilleEtDePays(
-			@RequestParam String city, @RequestParam String country)
-			throws CityNotFoundException {
-		return getWeatherPredictionFromAPI(city, country);
-	}
-
-	@CrossOrigin(origins = "http://localhost:8080")
-	@RequestMapping(value = "/byCoordinates")
-	public WeatherPrediction listPredictionParCoordonnees(@RequestParam String lat,
-			@RequestParam String lng) throws CityNotFoundException {
-
-		// Appel de l'API distante pour recuperer les informations nom de ville et de pays
-		String arguments = "&latlng=" + lat + "," + lng;
-		String request = urlGoogleMapsAPI + keyGoogleMaps + arguments;
-		System.out.println(request);
-
-		RestTemplate restTemplate = new RestTemplate();
-		Coordinates coordinates = restTemplate.getForObject(request, Coordinates.class);
-
-		String city = coordinates.getCity();
-		String country = coordinates.getCountry();
-
-		if (isEmpty(city)) {
-			throw new CityNotFoundException("No city found for these coordinates");
-		}
-
-		return getWeatherPredictionFromAPI(city, country);
-	}
-
-	// Appel de l'API distante pour recuperer les informations meteorologiques
 	/**
 	 * Take a <b>city</b> and a <b>country</b> as parameters and will call the weather API to get forecast on the five
 	 * next days.
@@ -65,16 +30,50 @@ public class HomeController {
 	 * @param country
 	 * @return WeatherPrediction
 	 */
-	private WeatherPrediction getWeatherPredictionFromAPI(String city, String country) {
-
+	@CrossOrigin(origins = "http://localhost:8080")
+	@RequestMapping(value = "/byCityAndCountry")
+	public WeatherPrediction listPredictionParNomDeVilleEtDePays(@RequestParam String city,
+			@RequestParam String country) throws CityNotFoundException {
 		String arguments = "&q=" + city + "," + country;
 		String request = urlWeatherAPI + keyWeatherAPI + arguments;
 
 		System.out.println(request);
 
 		RestTemplate restTemplate = new RestTemplate();
-		WeatherPrediction weather = restTemplate.getForObject(request,
-				WeatherPrediction.class);
+		WeatherPrediction weather = restTemplate.getForObject(request, WeatherPrediction.class);
+
+		weather.calcMinMax();
+
+		return weather;
+	}
+
+	/**
+	 * Take a <b>lat</b> and a <b>lng</b> as parameters and will call the weather API to get forecast on the five next
+	 * days.
+	 * 
+	 * @param lat
+	 * @param lng
+	 * @return WeatherPrediction
+	 */
+	@CrossOrigin(origins = "http://localhost:8080")
+	@RequestMapping(value = "/byCoordinates")
+	public WeatherPrediction listPredictionParCoordonnees(@RequestParam String lat, @RequestParam String lng)
+			throws CityNotFoundException {
+
+		// Appel de l'API distante pour recuperer les informations
+		String arguments = "&lat=" + lat + "&lon=" + lng;
+		String request = urlWeatherAPI + keyWeatherAPI + arguments;
+
+		RestTemplate restTemplate = new RestTemplate();
+		WeatherPrediction weather = restTemplate.getForObject(request, WeatherPrediction.class);
+
+		if (StringUtils.isEmpty(weather.getCity().getName())) {
+			DecimalFormat df = new DecimalFormat("####.##");
+			df.setRoundingMode(RoundingMode.CEILING);
+			weather.getCity()
+					.setName("lat=" + df.format(Double.valueOf(lat)) + "\nlon=" + df.format(Double.valueOf(lng)));
+			weather.getCity().setCountry("Unknown");
+		}
 
 		weather.calcMinMax();
 
--- src/test/java/com/weatherapp/WeatherForecastApplicationTests.java
+++ src/test/java/com/weatherapp/WeatherForecastApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = WeatherForecastApplication.class)
+@ContextConfiguration
 @WebAppConfiguration
 public class WeatherForecastApplicationTests {
 
