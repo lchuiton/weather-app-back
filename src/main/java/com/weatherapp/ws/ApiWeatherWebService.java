42d8de0495a3c0d923de97a8ae02c406b87871d6
lchuiton <lchuiton@lchuiton.com>
2017-12-25 21:50:49 -0500

    Refactoring the way OpenWeather is called

--- src/main/java/com/weatherapp/ws/ApiWeatherWebService.java
+++ src/main/java/com/weatherapp/ws/ApiWeatherWebService.java
@@ -1,16 +1,42 @@
 package com.weatherapp.ws;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 
 import com.weatherapp.model.WeatherPrediction;
 
 public class ApiWeatherWebService {
+	@Value("${api.weather.uri}")
+	private String apiWeatherUri;
 
 	@Autowired
 	RestTemplate restTemplate;
 
-	public WeatherPrediction callApiWeather(String request) {
+	public WeatherPrediction callApiWeather(Map<String, String> uriVariables) {
+		String request = buildRequestUri(uriVariables);
+
 		return restTemplate.getForObject(request, WeatherPrediction.class);
 	}
+
+	private String buildRequestUri(Map<String, String> uriVariables) {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(apiWeatherUri);
+
+		if (!uriVariables.isEmpty()) {
+			stringBuilder.append("&");
+		}
+
+		for (String variableKey : uriVariables.keySet()) {
+			stringBuilder.append(variableKey);
+			stringBuilder.append("=");
+			stringBuilder.append(uriVariables.get(variableKey));
+		}
+
+		return stringBuilder.toString();
+
+	}
+
 }
